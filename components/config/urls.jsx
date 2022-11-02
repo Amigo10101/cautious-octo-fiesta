3a3d84e13daba9df73179fb5032da53481f24d31
Amigo10101 <amigos10101@gmail.com>
2022-11-02 14:10:27 +0545

    New url

--- components/config/urls.jsx
+++ components/config/urls.jsx
@@ -1,15 +1,14 @@
-const url = process.env.API_URL;
 const API = {
-  getpackages: url + "packages/",
-  getcontent: url + "content/",
-  postcontent: url + "packages/editpackages/",
-  updatecontent: url + "packages/editpackages/",
-  deletepackage: url + "packages/delete_package/",
-  deletecontent: url + "packages/editpackages/",
-  getimages: url + "images/",
-  addimage: url + "images/add_image/",
-  deleteimage: url + "images/delete_image/",
-  savetodb: url + "images/save_to_db/",
+  getpackages: "/api/packages/",
+  getcontent: "/api/content/",
+  postcontent: "/api/packages/editpackages/",
+  updatecontent: "/api/packages/editpackages/",
+  deletepackage: "/api/packages/delete_package/",
+  deletecontent: "/api/packages/editpackages/",
+  getimages: "/api/images/",
+  addimage: "/api/images/add_image/",
+  deleteimage: "/api/images/delete_image/",
+  savetodb: "/api/images/save_to_db/",
 };
 
 export { API };
